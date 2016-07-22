e870b64345bb3163e55bccb5ff3dd7ca8de24ad1
stoffern <kristoffer.klevenberg@gmail.com>
2016-07-22 07:30:37 +0200

    api.call

--- src/API.js
+++ src/API.js
@@ -21,11 +21,24 @@ class Connection {
       if (this.endPoint.length < 5 || !this.endPoint) reject('Error: No endPoint set!')
       if (userObj.latitude == 0 || userObj.longitude == 0) reject ('Error: position missing')
 
+
+      if (this.endPoint == API_URL){
+        var RequestType = POGOProtos.Networking.Requests.RequestType
+        var req = this._serializeRequest([
+          RequestType('GET_PLAYER')
+          RequestType('GET_HATCHED_EGGS')
+          RequestType('GET_INVENTORY')
+          RequestType('CHECK_AWARDED_BADGES')
+          RequestType('DOWNLOAD_SETTINGS')
+        ])
+      }else{
+        var req = this._serializeRequest(reqs)
+      }
       // set requests
-      var req = this._serializeRequest(reqs)
       // set header
       var request = this._serializeHeader(req, userObj)
 
+      console.log(request)
       //create buffer
       var protobuf = request.encode().toBuffer();
 
@@ -37,87 +50,110 @@ class Connection {
             'User-Agent': 'Niantic App'
         }
       }
-
-      this.request.post(options, (err, response, body) => {
-          if (response === undefined || body === undefined) {
-              console.error('[!] RPC Server offline');
-              reject('RPC Server offline');
-          }
-          try {
-              var res = POGOProtos.Networking.Envelopes.ResponseEnvelope.decode(body);
-          } catch (e) {
-              if (e.decoded) { // Truncated
-                  console.warn(e);
-                  res = e.decoded; // Decoded message with missing required fields
-              }
-          }
-
-          //set endPoint
-          console.log(res.endPoint)
-          if (res.endPoint) this.endPoint == res.endPoint
-
-          //we have response (returns = response we want.. now lets parse it)
-          if (res.returns){
-            // response ={}
-            // reqs.map( (req,key) => {
-            //   //setFileName 
-            //   var ResponseType = ''
-            //   req = req.split("_")
-            //   req.map( word => {
-            //     ResponseType += _.upperFirst(_.toLower(word))
-            //   })
-            //   ResponseType += 'Response'
-
-            //   var proto = ProtoBuf.loadProtoFile({ root: "./src/", file: "POGOProtos/Networking/Responses/"+ResponseType+".proto" }).build("POGOProtos")
-            //   response[req] = proto.Networking.Responses[ResponseType].decode(res.returns[key])
-            //   console.log(response[req])
-            // })
+      // console.log(request)
 
 
+      // this.request.post(options, (err, response, body) => {
+      //     if (response === undefined || body === undefined) {
+      //         console.error('[!] RPC Server offline');
+      //         reject('RPC Server offline');
+      //     }
+      //     try {
+      //         var res = POGOProtos.Networking.Envelopes.ResponseEnvelope.decode(body);
+      //     } catch (e) {
+      //         if (e.decoded) { // Truncated
+      //             console.warn(e);
+      //             res = e.decoded; // Decoded message with missing required fields
+      //         }
+      //     }
 
-            var profile = POGOProtos.Networking.Responses.GetPlayerResponse.decode(res.returns[0])
-            console.log(profile)
-          }
-          if(res) 
-            resolve(res);
-          else 
-            resolve(this._request(userObj.endPoint, userObj.accessToken, req))
+      //     //set endPoint
+      //     console.log(res.endPoint)
+      //     if (res.endPoint) this.endPoint == res.endPoint
 
-      });
+      //     //we have response (returns = response we want.. now lets parse it)
+      //     if (res.returns){
+      //       // response ={}
+      //       // reqs.map( (req,key) => {
+      //       //   //setFileName 
+      //       //   var ResponseType = ''
+      //       //   req = req.split("_")
+      //       //   req.map( word => {
+      //       //     ResponseType += _.upperFirst(_.toLower(word))
+      //       //   })
+      //       //   ResponseType += 'Response'
+
+      //       //   var proto = ProtoBuf.loadProtoFile({ root: "./src/", file: "POGOProtos/Networking/Responses/"+ResponseType+".proto" }).build("POGOProtos")
+      //       //   response[req] = proto.Networking.Responses[ResponseType].decode(res.returns[key])
+      //       //   console.log(response[req])
+      //       // })
+
+
+
+      //       var profile = POGOProtos.Networking.Responses.GetPlayerResponse.decode(res.returns[0])
+      //       console.log(profile)
+      //     }
+      //     if(res) 
+      //       resolve(res);
+      //     else 
+      //       resolve(this._request(userObj.endPoint, userObj.accessToken, req))
+
+      // });
 
     })
+  }
+
+  getEndPoint(){
+
   }
   _serializeRequest(reqs){
     var res = []
     reqs.map( req => {
       var reqId = POGOProtos.Networking.Requests.RequestType[req]
-      console.log(reqId)
       res.push(new POGOProtos.Networking.Requests.Request({'request_type': reqId}))
     })
     return res
   }
 
   _serializeHeader(req, userObj){
-    var env = {
-      'status_code': 2,
-      'request_id': 8145806132888207460,
-      'latitude': userObj.latitude,
-      'longitude': userObj.longitude,
-      'altitude': userObj.altitude,
-      'unknown12': 989,
-      'requests': req,
-      'auth_info': new POGOProtos.Networking.Envelopes.RequestEnvelope.AuthInfo({
-        'provider': userObj.provider,
-        'token': new POGOProtos.Networking.Envelopes.RequestEnvelope.AuthInfo.JWT({
-          'contents': userObj.accessToken,
-          'unknown2': 59,
-        })
-      })
+
+    var tmpRequest = {
+      'status_code':   2,
+      'request_id': 1469378659230941192,
+      'requests':   req,
+      'latitude':   userObj.latitude,
+      'longitude':  userObj.longitude,
+      'altitude':   userObj.altitude,
+      'unknown12':  989
     }
+    // console.log(tmpRequest)
+    tmpRequest.auth_info = new POGOProtos.Networking.Envelopes.RequestEnvelope.AuthInfo({
+      'provider': 'ptc',
+      'token': new POGOProtos.Networking.Envelopes.RequestEnvelope.AuthInfo.JWT({
+        'contents': userObj.accessToken,
+        'unknown2': 59
+      })
+    });
 
-    if (this.auth_ticket !== null) env.auth_ticket = this.auth_ticket
+    // var env = {
+    //   status_code: 2,
+    //   request_id: 1469378659230941192,
+    //   latitude: userObj.latitude,
+    //   longitude: userObj.longitude,
+    //   altitude: userObj.altitude,
+    //   unknown12: 989,
+    //   requests: req,
+    //   auth_info: new POGOProtos.Networking.Envelopes.RequestEnvelope.AuthInfo({
+    //     provider: userObj.provider,
+    //     token: new POGOProtos.Networking.Envelopes.RequestEnvelope.AuthInfo.JWT({
+    //       contents: userObj.accessToken,
+    //       unknown2: 59,
+    //     })
+    //   })
+    // }
 
-    return new POGOProtos.Networking.Envelopes.RequestEnvelope(env);
+    // if (this.auth_ticket !== null) env.auth_ticket = this.auth_ticket
+    return new POGOProtos.Networking.Envelopes.RequestEnvelope(tmpRequest);
   }
 
   _setAuthTicket(body){
