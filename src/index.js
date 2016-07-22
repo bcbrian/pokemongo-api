17446508e9ec0b9f92ec00accabab18d4967fbc0
Jimmy Wärting <jimmy@warting.se>
2016-07-22 13:01:11 +0200

    call the Call fn

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ class PokemonGOAPI {
     this.api = new API()
     this.logged = false
     this.debug = true
-
   }
 
   async login(username, password, location, provider) {
@@ -16,6 +15,7 @@ class PokemonGOAPI {
     if (provider !== 'ptc' && provider !== 'google') {
       throw new Error('Invalid provider')
     }
+
     this.player.provider = provider
 
     await this.player.setLocation(location)
@@ -28,30 +28,27 @@ class PokemonGOAPI {
   //
   //This calls the API direct
   //
-  async Call(req) {
-    let res = await this.api.Request(req, this.player.playerInfo)
-    return res
+  Call(req) {
+    return this.api.Request(req, this.player.playerInfo)
   }
-  async GetInventory(){
-    let res = await this.Call[{ request: 'GET_INVENTORY' }]
-    console.log('res')
-    console.log(res)
-    return res
+
+  GetInventory(){
+    return this.Call([{ request: 'GET_INVENTORY' }])
   }
-  async GetPlayer(){
-    let res = await this.Call[{ request: 'GET_PLAYER' }]
-    return res
+
+  GetPlayer(){
+    return this.Call([{ request: 'GET_PLAYER' }])
   }
 
   //
   // HeartBeat
   //
-
   async ToggleHartBeat(){
     this.useHartBeat = !this.useHartBeat
     this._loopHartBeat()
     return this.useHartBeat
   }
+
   async _loopHartBeat(){
     while(this.useHartBeat){
       var finalWalk = [];
@@ -60,21 +57,17 @@ class PokemonGOAPI {
       });
       var nullarray = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 
-      let res = await this.Call[{ 
+      let res = await this.Call[{
         request: 'GET_MAP_OBJECTS',
         message: {
-          cell_id: 
+          cell_id: '???',
           since_timestamp_ms: nullarray,
           latitude: this.player.playerInfo.latitude,
           longitude: this.player.playerInfo.longitude,
-        } 
+        }
       }]
     }
   }
-
-
-
-
 
 }
 
