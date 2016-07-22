3f50a3cf1b468343f26b225d5380194f5e0cbb97
stoffern <kristoffer.klevenberg@gmail.com>
2016-07-22 14:43:23 +0200

    CatchPokemon() EncounterPokemon() -> ok

--- src/index.js
+++ src/index.js
@@ -69,6 +69,43 @@ class PokemonGOAPI {
     }
   }
 
+  async CatchPokemon(pokemon, spawn_id){
+    // TODO 
+    //add checks for input 
+    
+    var spin_modifier 0.85 + Math.random() * 0.15 
+
+    let res = await this.Call[{
+      request: 'ENCOUNTER',
+      message: {
+        encounter_id: pokemon.encounter_id,
+        pokeball: 1,
+        normalized_reticle_size: 1.95,
+        spawn_point_guid: pokemon.spawn_point_id,
+        hit_pokemon: true,
+        spin_modifier: spin_modifier,
+        normalized_hit_position: 1.0,
+      }
+    }]
+    return res
+  }
+
+  async EncounterPokemon(enc_id, spawn_id){
+    // TODO 
+    //add checks for input 
+
+    let res = await this.Call[{
+      request: 'ENCOUNTER',
+      message: {
+        encounter_id: enc_id,
+        spawn_point_id: spawn_id,
+        latitude: this.player.playerInfo.latitude,
+        longitude: this.player.playerInfo.longitude,
+      }
+    }]
+    return res
+  }
+
 }
 
 export default PokemonGOAPI
